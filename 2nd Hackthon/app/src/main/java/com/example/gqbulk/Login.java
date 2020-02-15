42a78493967a2afda8b7d1a128bd208ea20bb6bc
yx <angyongxin@hotmail.com>
2020-02-15 18:23:30 +0800

    ssa

---
+++ 2nd Hackthon/app/src/main/java/com/example/gqbulk/Login.java
@@ -0,0 +1,63 @@
+package com.example.gqbulk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class Login extends AppCompatActivity {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+          btn=(Button)findViewById(R.id.Lbutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText LUsername = (EditText)findViewById(R.id.LUsernameET);
+
+             /*   Toast toast= Toast. makeText(getApplicationContext(),LUsername.getText(),Toast. LENGTH_SHORT);
+                toast. show();*/
+
+                db.collection("Users")
+                        .whereEqualTo("LoginID", LUsername.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("GDQUID", "DocumentSnapshot successfully updated!"+document.getData());
+                                        String name=document.getString("Name");
+                                        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+                                        startActivity(myIntent);
+                                }
+
+                                } else {
+                                    Log.d("error", "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+
+
+            }
+        });
+    }
+}
--- 2nd Hackthon/app/src/main/java/com/example/gqbulk/MainActivity.java
+++ 2nd Hackthon/app/src/main/java/com/example/gqbulk/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.gqbulk;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btnDirect);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), Login.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
